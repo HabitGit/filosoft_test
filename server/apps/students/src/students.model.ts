c97b904645c9028b985729b34c376d014e3ae63e
Habit <char.habit@gmail.com>
2023-11-29 21:23:47 +0400

    Done

--- server/apps/students/src/students.model.ts
+++ server/apps/students/src/students.model.ts
@@ -1,25 +1,21 @@
 import {Column, DataType, HasMany, Model, Table} from "sequelize-typescript";
 import {Grades} from "./grades.model";
 
-@Table({ tableName: 'students' })
+@Table({tableName: 'students'})
 export class Students extends Model<Students> {
     @Column({
-        type: DataType.BIGINT,
+        type: DataType.STRING,
         unique: true,
-        autoIncrement: true,
         primaryKey: true,
     })
-    id: number;
-
-    @Column({ type: DataType.STRING })
     personalCode: string;
 
-    @Column({ type: DataType.STRING })
+    @Column({type: DataType.STRING})
     name: string;
 
-    @Column({ type: DataType.STRING })
+    @Column({type: DataType.STRING})
     lastName: string;
 
     @HasMany(() => Grades)
-    grades: Grades[];
+    statistic: Grades[];
 }
